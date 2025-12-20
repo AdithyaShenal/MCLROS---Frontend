cad08bdd5e95a60e51b275a18d408f659526a784
DNishadini <dinushikaherath150@gmail.com>
2025-12-20 23:40:08 +0530

    create production page

--- src/pages/ProductionPage.tsx
+++ src/pages/ProductionPage.tsx
@@ -1,5 +1,18 @@
+import {useState} from "react";
 const ProductionPage = () => {
-  return <div>ProductionPage</div>;
+  const[search,setSearch] = useState("");
+
+  return (
+    <div className="p-7 w-full">
+      <h1 className="text-2xl font-semibold">Production Overview</h1>
+      <p className="text-gray-500 text-sm mb-6">
+        Manage Pending collection requests
+      </p>
+      <div className="flex flex-wrap items-center gap-4 mb-6">
+        <input type="text" placeholder="Search by farmer name..." className="input input-bordered w-full max-w-md" value={search} onChange={(e) => setSearch(e.target.value)}/>
+      </div>
+    </div>
+  );
 };
 
 export default ProductionPage;
