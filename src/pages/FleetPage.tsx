3332b98d7125822ae91cfeb50b19d2d240882cf5
DNishadini <dinushikaherath150@gmail.com>
2025-12-24 10:49:20 +0530

    create heading of the fleet ui

--- src/pages/FleetPage.tsx
+++ src/pages/FleetPage.tsx
@@ -1,5 +1,18 @@
+import { use, useState } from "react";
+import { Search } from "lucide-react";
+
 const FleetPage = () => {
-  return <div>FleetPage</div>;
+  const [activeTab, setActiveTab] = useState<"Trucks" | "Drivers">("Drivers");
+  const [search, setSearch] = useState("");
+  const [truck, setTruck] = useState("");
+  return (
+    <div className="p-7 w-full">
+      <h1 className="text-2xl font-semibold">Fleet Management</h1>
+      <p className="text-gray-500 text-sm mb-4">
+        Manage your trucks and drivers
+      </p>
+    </div>
+  );
 };
 
 export default FleetPage;
